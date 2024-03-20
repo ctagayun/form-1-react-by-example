30d3e62adabed830dfcad1d9c06b2e1baf3dc0dd
Domingo <30532920+ctagayun@users.noreply.github.com>
2024-03-20 10:10:14 -0500

    Save

--- src/forms/loginForm.jsx
+++ src/forms/loginForm.jsx
@@ -10,26 +10,46 @@ Login form functional component
   - For preventing the native browser behavior (which would 
     perform a refresh on the browser), we can use the 
     preventDefault() method on the form's event.
+
+  - Whenever we want to access/read an HTML element in JSX, we 
+    would be using React's useRef Hook:
 */
 
 import * as React from 'react';
 
 const LoginForm = () =>{
+
+  //whenever we want to access a HTML element in JSX, 
+  //we would be using React's useRef Hook:
+  const emailRef = React.useRef();
+  const passwordRef = React.useRef();
+
     const handleSubmit = (event) => {
       event.preventDefault();
+      //Reading value of the textbox. Be sure  to add
+      //"ref" (e.g. emailRef) to the textbox. 
+      //const email = emailRef.current.value
+      //const password = passwordRef.current.value
+       
+      //The lazy aproach is to read the value directly in
+      //the event handler
+      const email = event.target.elements.email.value;
+      const password = event.target.elements.password.value;
+
+      alert(email + ' ' + password);
     }
 
     return(
       <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="email">Email</label>
-           <input id="email" type="text" />
+           <input id="email" type="text" ref={emailRef}/>
         </div>
         <div>
           <label htmlFor="password">Password</label>
-           <input id="password" type="password" />
+           <input id="password" type="password" ref={passwordRef} />
         </div>
-       <button type="sumit">Login</button>
+       <button type="submit">Login</button>
       </form>
     ); 
  
